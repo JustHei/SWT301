849b42ac50ec5d653923d93110296cd5fe41133c
Thanhh Phucc <116250948+JustHei@users.noreply.github.com>
2024-06-11 16:45:24 +0700

    Update main.js

--- build/web/js/main.js
+++ build/web/js/main.js
@@ -208,12 +208,13 @@
     proQty.on('click', '.qtybtn', function () {
         var $button = $(this);
         var oldValue = $button.parent().find('input').val();
+	var newVal;
         if ($button.hasClass('inc')) {
-            var newVal = parseFloat(oldValue) + 1;
+        	newVal = parseFloat(oldValue) + 1;
         } else {
             // Don't allow decrementing below zero
             if (oldValue > 0) {
-                var newVal = parseFloat(oldValue) - 1;
+                newVal = parseFloat(oldValue) - 1;
             } else {
                 newVal = 0;
             }
